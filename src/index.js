4f03920c9185fe82047e3d12f084494bbe51a992
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-06-28 18:16:42 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { App } from './components/App';
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename='/sword_art_online'>
