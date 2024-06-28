f55a80a2f9b2c685b310aa7eae3f43dcbfbc2ce1
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-06-28 18:11:37 +0200

    Changes

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from './components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/sword_art_online_two'>
+    <BrowserRouter basename='/sword_art_online'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
